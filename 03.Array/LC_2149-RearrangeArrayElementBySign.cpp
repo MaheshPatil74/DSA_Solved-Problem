3755b83aad7cc7b4e3c76c38b1ea8e0bd7270173
Mahesh <maheshpatil3925@gmail.com>
2026-03-04 23:13:04 +0530

    Add solution for LC 2149 Rearrange Array Elements by Sign and update Tree Implementation

---
+++ 03.Array/LC_2149-RearrangeArrayElementBySign.cpp
@@ -0,0 +1,21 @@
+// Approach 1 : Using extra array
+//  TC : O(N) , SC : O(N) // requires extra array for stable solution
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int i = 0 ;
+        int j = 1 ;
+        vector<int> res( nums.size() ) ;
+        for( int k = 0 ; k<nums.size() ; k++  ){
+            if( nums[k] > 0 ){
+                res[i] = nums[k] ;
+                i+=2 ;
+            }
+            else{
+                res[j] = nums[k] ;
+                j+=2 ;
+            }
+        }
+        return res ;
+    }
+};
--- 14.Binary Tree/demoTree.cpp
+++ 14.Binary Tree/demoTree.cpp
@@ -3,123 +3,50 @@
 using namespace std ;
 
 class node{
-    public:
+    public : 
         int data ;
-        node * left ;
-        node * right ;
-
-    node(int data){
-        this->data = data ;
-        this->left = NULL ;
-        this->right = NULL ;
-    }
+        node* left ;
+        node* right ;
+    
+        node( int data){
+            this->data = data ;
+            this->left = NULL ;
+            this->right = NULL ;
+        }   
 };
 
-node * buildTree( node * root){
-    cout<<"Enter the data : ";
+node* buildTree( node* root ){
+    cout<<"Enter The Data : ";
     int data ;
     cin>>data ;
-    root = new node(data) ;
+    root = new node(data) ; 
+    if( data == -1 )
+        return NULL ;
 
-    if( data == -1 ){
-        return NULL ; 
-    }
-
-    cout<<"Enter data for inserting left of "<<data<<endl;
-    root->left = buildTree(root->left) ;
-    cout<<"Enter data for inserting right of "<<data<<endl;
-    root->right = buildTree(root->right) ;
+    cout<<"Enter The data for inserting left of "<<data<<endl ;
+    root->left = buildTree( root->left );
+    cout<<"Enter The data for inserting Right of "<<data<<endl ;
+    root->right = buildTree( root->right );
     return root ;
 }
 
-void Inorder(node * root ){
-    if( root == NULL ){
-        return ;
-    }
-    Inorder(root->left) ;
-    cout<< root->data <<" " ;
-    Inorder(root->right) ;
-}
-
-void Preorder(node * root ){
-    if( root == NULL ){
-        return ;
-    }
-    cout<< root->data <<" " ;
-    Preorder(root->left) ;
-    Preorder(root->right) ;
-}
-
-void Postorder(node * root ){
-    if( root == NULL ){
-        return ;
-    }
-    Postorder(root->left) ;
-    Postorder(root->right) ;
-    cout<< root->data <<" " ;
-}
-
-void levelOrderTraversal(node * root){
+void levelOrderTraversal( node * root ){
     queue<node*> q ;
-    q.push(root) ;
-    q.push(NULL) ;
+    q.push( root ) ;
+    q.push( NULL ) ;
 
     while( !q.empty() ){
-        node * temp = q.front() ;
+        node* temp = q.front() ;
         q.pop() ;
-
-        if(temp==NULL){
-            cout<<endl ;
-            if( !q.empty() ){
-                q.push(NULL) ;
-            }
-        }
-        else{
-            cout<<temp->data<<" ";
-            if( temp->left ){
-                q.push(temp->left) ;
-            }
-            if( temp->right ){
-                q.push(temp->right) ;
-            }
-        }
+        
     }
 }
 
-void buildFromLevelOrder( node * & root ){
-    queue<node * > q ;
-    cout<<"Enter the data for Root : ";
-    int data ;
-    cin>>data ;
-    root = new node(data) ;
-    q.push(root) ;
-    while( !q.empty() ){
-        node * temp = q.front() ;
-        q.pop() ;
-
-        cout<<"Enter Left Node For "<<temp->data<<endl ;
-        int leftData ;
-        cin>>leftData ;
-        if(leftData != -1 ){
-            temp->left = new node(leftData) ;
-            q.push(temp->left) ;
-        }
-
-        cout<<"Enter right Node For "<<temp->data<<endl ;
-        int rightData ;
-        cin>>rightData ;
-        if(rightData != -1 ){
-            temp->right = new node(rightData) ;
-            q.push(temp->right) ;
-        }
-    }
-}
 
 int main(){
     node * root = NULL ;
-    /*
     root = buildTree(root) ;
-
+    /*
     // Inorder Traversal of Tree
     cout<<"Inorder Traversal of Tree : " ;
     Inorder(root) ;
@@ -134,11 +61,11 @@ int main(){
     cout<<"Postorder Traversal of Tree : ";
     Postorder(root) ;
     cout<<endl ;
+    */
 
     // Level order Traversal
     cout <<endl<< "Printing the level order traversal output : " << endl;
     levelOrderTraversal(root) ;
-    */
 
     buildFromLevelOrder(root) ;
     cout <<endl<< "Printing the level order traversal output : " << endl;
