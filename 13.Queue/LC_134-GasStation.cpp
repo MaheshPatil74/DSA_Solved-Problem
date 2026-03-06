50d328ee11c1c5d7bf1fbda241e996bd7e18dfaf
Mahesh <maheshpatil3925@gmail.com>
2026-03-06 10:37:24 +0530

    Solve LC 134 Gas Station

---
+++ 13.Queue/LC_134-GasStation.cpp
@@ -0,0 +1,19 @@
+// TC : O(N) , SC : O(1)
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int deficit = 0 ;
+        int balance = 0 ;
+        int start = 0 ;
+
+        for( int i = 0 ; i<gas.size() ; i++ ){
+            balance = balance + gas[i] - cost[i] ;
+            if( balance < 0 ){
+                start = i+1 ;
+                deficit += balance ;
+                balance = 0 ;
+            }
+        }
+        return ( balance + deficit >= 0 ) ? start : -1 ;
+    }
+};
