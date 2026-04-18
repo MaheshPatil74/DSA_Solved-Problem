8e44c714bad14128a18beb60ca1fb2395e7e5853
Mahesh <maheshpatil3925@gmail.com>
2026-04-18 23:32:06 +0530

    Update Code

--- 14.Binary Tree/L5GG_CreateTreefromInorderAndPostorder.cpp
+++ 14.Binary Tree/L5GG_CreateTreefromInorderAndPostorder.cpp
@@ -1,20 +1,5 @@
-/* Tree node structure
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};*/
-
 class Solution {
   private :
-  
     void createMapping(vector<int> inorder, map<int,int> & nodeToIndex , int n){
         for(int i = 0 ; i<n ; i++)
             nodeToIndex[inorder[i]] = i ;
@@ -22,9 +7,8 @@ class Solution {
     
     Node* solve(vector<int> inorder, vector<int> postorder, int & index , int inorderStart , int inorderEnd , int n , map<int,int> & nodeToIndex){
         // base case
-        if(index<0 || inorderStart>inorderEnd ){
+        if(index<0 || inorderStart>inorderEnd )
             return NULL ;
-        }
         
         // create a root node for element
         int element = postorder[index--] ;
@@ -36,14 +20,12 @@ class Solution {
         // recursive call
         root->right = solve(inorder,postorder,index,position+1,inorderEnd,n,nodeToIndex) ;
         root->left = solve(inorder,postorder,index,inorderStart,position-1,n, nodeToIndex) ;
-        
         return root ;
     }
   public:
 
     // Function to return a tree created from postorder and inoreder traversals.
     Node* buildTree(vector<int> inorder, vector<int> postorder) {
-        // code here
         int n = inorder.size() ;
         int postOrderIndex = n-1 ;
         map<int,int> nodeToIndex ;
@@ -53,6 +35,5 @@ class Solution {
         
         Node* ans = solve(inorder,postorder,postOrderIndex,0,n-1,n,nodeToIndex) ;
         return ans ;
-        
     }
 };
