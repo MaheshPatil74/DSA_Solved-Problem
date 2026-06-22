8a3f5b7df23483cb538a75b799d0a1adaaba909f
Mahesh <maheshpatil3925@gmail.com>
2026-06-22 16:36:40 +0530

    Leetcode daily problem add

---
+++ 04.String And Character Array/LC_1189-MaxNumberOfBalloon.cpp
@@ -0,0 +1,17 @@
+// TC : O(N) , SC : O(26) == O(1)
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> freq(26,0) ;
+        for( char ch : text )
+            freq[ch-'a']++ ; 
+
+        return min({
+            freq[ 'b' - 'a'] ,
+            freq[ 'a' - 'a'] ,
+            freq[ 'n' - 'a'] ,
+            freq[ 'l' - 'a'] / 2 ,
+            freq[ 'o' - 'a'] / 2
+        }) ;
+    }
+};
